81f3acd6309af369dd04e8dfcca346480e5e035d
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2019-01-31 12:58:58 +0500

    data being recorded

--- tsp.py
+++ tsp.py
@@ -317,8 +317,8 @@ def main():
         for childGeneration in range (nChildren//2):
             # choosing parents 
             #parentsIndex = fitnessProportionalSelection(fitness, 2)
-            #parentsIndex = rankbasedSelection(fitness, 2 )
-            parentsIndex = binaryTournament(fitness, 2)
+            parentsIndex = rankbasedSelection(fitness, 2 )
+            #parentsIndex = binaryTournament(fitness, 2)
             #parentsIndex = randomSelection(fitness, 2)
             #parentsIndex = truncation(fitness, 2)
             
@@ -334,11 +334,11 @@ def main():
                 
             # select new population        
             #populationIndices = fitnessProportionalSelection(fitness, nPopulation)
-            #populationIndices = rankbasedSelection(fitness, nPopulation)
+            populationIndices = rankbasedSelection(fitness, nPopulation)
             #populationIndices = binaryTournament(fitness, nPopulation)
             #populationIndices = randomSelection(fitness, nPopulation)
-            populationIndices = truncation(fitness, nPopulation)
-            print(len(populationIndices))
+            #populationIndices = truncation(fitness, nPopulation)
+            #print(len(populationIndices))
 
             tempPopulation = []
             tempFitness = []
@@ -386,7 +386,7 @@ def Fullmain():
         avg_minAllIterations.append(sumMin / nIterations)
 
     #collected data
-    '''
+    
     print("generation# Run#1.Average Run#2.Average Run#3.Average Run#4.Average Run#5.Average Run#6.Average Run#7.Average Run#8.Average Run#9.Average Run#10.Average Average.Average")
     for i in range (len(minAllIterations[0])):
         print(i , avgAllIterations[0][i],avgAllIterations[1][i],avgAllIterations[2][i],avgAllIterations[3][i],avgAllIterations[4][i],avgAllIterations[5][i],avgAllIterations[6][i],avgAllIterations[7][i],avgAllIterations[8][i],avgAllIterations[9][i],avg_avgAllIterations[i])
@@ -395,7 +395,7 @@ def Fullmain():
     print("generation# Run#1.BFS Run#2.BFS Run#3.BFS Run#4.BFS Run#5.BFS Run#6.BFS Run#7.BFS Run#8.BFS Run#9.BFS Run#10.BFS Average.BFS")
     for i in range (len(minAllIterations[0])):
         print(i , minAllIterations[0][i],minAllIterations[1][i],minAllIterations[2][i],minAllIterations[3][i],minAllIterations[4][i],minAllIterations[5][i],minAllIterations[6][i],minAllIterations[7][i],minAllIterations[8][i],minAllIterations[9][i],avg_minAllIterations[i])
-    '''
+    
     plotGraph(len(avg_avgAllIterations),avg_avgAllIterations, avg_minAllIterations)
 
 
